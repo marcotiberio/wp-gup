4cba9255f8f8b76bce096c7fcbb94f4c919b4b8c
Marco Tiberio <tiberiomarco01@gmail.com>
2020-03-16 11:15:31 +0100

    footer

--- footer.php
+++ footer.php
@@ -14,6 +14,9 @@
 	</div><!-- #content -->
 
 	<footer id="colophon" class="site-footer">
+		<div class="footer-custom">
+			GUP Magazine
+		</div>
 		<div class="site-info">
 			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'gup_underscore' ) ); ?>">
 				<?php
