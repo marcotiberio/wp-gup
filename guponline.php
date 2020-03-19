3f05d9453148ca74b33a9d9697b8d60ee249d645
Marco Tiberio <tiberiomarco01@gmail.com>
2020-03-19 14:27:01 +0100

    Update guponline.php

--- guponline.php
+++ guponline.php
@@ -6,6 +6,7 @@ get_header();
 <section class="magazine-page" id="page1-2">
     <div class="magazine-page-image"></div>
     <div class="magazine-page-article">
+    <h3>Kowalski set out to capture snow-covered terrain in Poland from the sky</h3>
             <div>
             <p>Having removed himself from the earth, both physically and mentally, he floats over the country and from his open cockpit (or sometimes paragliding) he is faced with the ambiguous forms and shapes as seen from such an altitude. From this extreme vantage point, the landscape transgresses his referential horizon, his primordial ‘radar’ gets a turbo-boost, and his being transforms into another ‘Dasein’ – which, as a concept, literally means ‘Being-there’.
             The philosopher Martin Heidegger (1889–1976) initially introduced the German expression ‘Dasein’ as the essence of being human, as an individual. It by now includes issues such as personhood, mortality and the dilemma or paradox of living in relationship with all that exists simultaneously with and in oneself. In short, ‘Dasein’ defines a kind of existential ‘wokeness’. From this lonely height, Kowalski started to ask himself: “Who am I? What am I doing here?” But also: “What would the world be like without culture, when everything that has been imposed on it has been erased?”
@@ -13,7 +14,6 @@ get_header();
             In the 1850s, when Gaspard-Félix Tournachon (1820–1910, better known under the alias Nadar) began taking photographs from a hot-air balloon, he described the sensation of ascending as a “free, calm, levitating into the silent immensity of welcoming and beneficent space”. The world seen from this position – as noted by Nadar – offers not only an admirable spectacle and an immense “carpet without edges”, but also “a purity of lines and an extraordinary clarity of sight with the exquisite impression of a marvellous, ravishing cleanliness!”</p>
             </div>
             <div>
-            <h3>Kowalski set out to capture snow-covered terrain in Poland from the sky</h3>
             <p>
             Ever since this pioneering aerial photography, photographers have been ‘scanning’ the earth, in an urge to map the world from above. Every square inch of the planet has been documented, it seems, with the support of airplanes and drones; pictures of each new discovery are examined and interpreted in every possible way. However, Kacper Kowalski’s latest work is rather uninformative.
             Indeed, Poland looks extraterrestrial when seen from the viewpoint of a bird – especially in winter, when the country is covered in snow. Just as the Rorschach test allows for subjective perceptions of inkblots so we assign a conventional meaning to a symbol (for example, we all agree that a small triangle pointing to the right will start a video or an audio file) the urge to ‘bend’ these visual abstractions towards the known is strong, while nevertheless leaving the onlooker in a state of oblivion.
