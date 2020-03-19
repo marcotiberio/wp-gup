1f90b62242168e1d2dedb5a0ae2a309b0ea44dd0
Marco Tiberio <tiberiomarco01@gmail.com>
2020-03-19 12:24:25 +0100

    pages

--- guponline.php
+++ guponline.php
@@ -2,9 +2,27 @@
 /* Template Name: Gup Online */
 ?>
 
-<section class="magazine-page">
-
+<section class="magazine-page" id="page1-2">
     <div>ciao</div>
     <div>ciao</div>
+</section>
 
+<section class="magazine-page" id="page3-4">
+    <div>ciao</div>
+    <div>ciao</div>
+</section>
+
+<section class="magazine-page" id="page5-6">
+    <div>ciao</div>
+    <div>ciao</div>
+</section>
+
+<section class="magazine-page" id="page7-8">
+    <div>ciao</div>
+    <div>ciao</div>
+</section>
+
+<section class="magazine-page" id="page9-10">
+    <div>ciao</div>
+    <div>ciao</div>
 </section>
