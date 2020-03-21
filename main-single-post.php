e4334d211f3fbcb4d4c6fc837fd040e67875e340
Marco Tiberio <tiberiomarco01@gmail.com>
2020-03-21 18:42:23 +0100

    restore link artist

--- main-single-post.php
+++ main-single-post.php
@@ -48,16 +48,7 @@
             </div>
             <div>
                 <h4>Artist Website</h4>
-                <p><?php 
-                    $link = get_field('link');
-                    if( $link ): 
-                        $link_url = $link['url'];
-                        $link_title = $link['title'];
-                        $link_target = $link['target'] ? $link['target'] : '_self';
-                        ?>
-                        <a class="button" href="<?php echo esc_url( $link_url ); ?>" target="<?php echo esc_attr( $link_target ); ?>"><?php echo esc_html( $link_title ); ?></a>
-                    <?php endif; ?>
-                </p>
+                <p><?php the_field('post_credits_artist_website'); ?></p>
             </div>
             <div>
                 <h4>Place</h4>
