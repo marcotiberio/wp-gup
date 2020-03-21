ae9e154dde92d7c8ad8783b4ba328a67b0a27bd4
Marco Tiberio <tiberiomarco01@gmail.com>
2020-03-21 16:32:42 +0100

    test post

--- main-single-post.php
+++ main-single-post.php
@@ -5,3 +5,23 @@
  */
   
  get_header();  ?>
+
+<div class="wrap">
+	
+	<div id="event-hero">
+
+    
+
+			<p><?php the_field('credits'); ?></p>
+			<p><?php the_field('article-text'); ?></p>
+
+			<img src="<?php the_field('hero_image'); ?>" />
+
+			<p><?php the_content(); ?></p>
+
+		
+	</div>
+	
+</div>
+
+<?php get_footer(); ?>
