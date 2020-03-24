981a2eb04069ca6f8164c42e891193c9b252f05b
Marco Tiberio <tiberiomarco01@gmail.com>
2020-03-24 16:45:03 +0100

    the excerpt

--- main-single-post.php
+++ main-single-post.php
@@ -94,7 +94,7 @@ $my_query->the_post();?>
         <a href="<? the_permalink()?>" rel="bookmark" title="<?php the_title(); ?>"><?php the_post_thumbnail(); ?></a>
         <div class="relatedcontent">
             <h3><a href="<? the_permalink()?>" rel="bookmark" title="<?php the_title(); ?>"><?php the_title(); ?></a></h3>
-            <p>ciao</p>
+            <p><?php the_excerpt(); ?></p>
         </div>
     </div>
 <?
