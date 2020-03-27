358352fbf3043be16aabf917625467b6fff5395c
Marco Tiberio <tiberiomarco01@gmail.com>
2020-03-27 18:35:39 +0100

    no slider plugin

--- page.php
+++ page.php
@@ -18,8 +18,6 @@ get_header();
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main">
 
-		<?php the_field('homepage_slider'); ?>
-
 		<?php
 		while ( have_posts() ) :
 			the_post();
