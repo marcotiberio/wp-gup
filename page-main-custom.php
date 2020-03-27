509da99bd19a87fa5e69f04e417a93eb2cc91188
Marco Tiberio <tiberiomarco01@gmail.com>
2020-03-27 09:43:17 +0100

    created page main custom

---
+++ page-main-custom.php
@@ -0,0 +1,29 @@
+<?php 
+	/* Template Name: Page Main */ 
+?>
+
+<?php get_header(); ?>
+
+	<div id="primary--page-main" class="content-area--page-main">
+		<main id="main--page-main" class="site-main--page-main">
+
+		<?php
+		while ( have_posts() ) :
+			the_post();
+
+			get_template_part( 'template-parts/content', 'page' );
+
+			// If comments are open or we have at least one comment, load up the comment template.
+			if ( comments_open() || get_comments_number() ) :
+				comments_template();
+			endif;
+
+		endwhile; // End of the loop.
+		?>
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php
+get_sidebar();
+get_footer();
