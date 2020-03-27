031296034b7913d57ee8171deb4cb97ef666a127
Marco Tiberio <tiberiomarco01@gmail.com>
2020-03-27 18:31:46 +0100

    homepage slider plugin

--- page.php
+++ page.php
@@ -18,6 +18,8 @@ get_header();
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main">
 
+		<?php the_field('homepage_slider'); ?>
+
 		<?php
 		while ( have_posts() ) :
 			the_post();
