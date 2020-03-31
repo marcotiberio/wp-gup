4227993df4bc365f592b250ec944a73acc03b4e3
Marco Tiberio <45846191+marcotiberio@users.noreply.github.com>
2020-03-31 18:45:23 +0200

    test landing page content

--- landing-page.php
+++ landing-page.php
@@ -2,6 +2,19 @@
 	/* Template Name: Landing Page */ 
 ?>
 
+    <?php
+		while ( have_posts() ) :
+			the_post();
+
+			get_template_part( 'template-parts/content', 'page' );
+
+			// If comments are open or we have at least one comment, load up the comment template.
+			if ( comments_open() || get_comments_number() ) :
+				comments_template();
+			endif;
+
+		endwhile; // End of the loop.
+	?>
 
     <section class="background_landing">
         <div class="embed--landing--container_desktop">
