f2462e3946e1764b8b414f173edc2fb7054dd167
Marco Tiberio <45846191+marcotiberio@users.noreply.github.com>
2020-03-31 18:33:17 +0200

    acf embed video

--- landing-page.php
+++ landing-page.php
@@ -4,5 +4,11 @@
 
 
     <section class="background_landing">
+        <div class="embed--landing--container_desktop">
+            <?php the_field('landing_video_desktop'); ?>
+        </div>
+        <div class="embed--landing--container_mobile">
+            <?php the_field('landing_video_mobile'); ?>
+        </div>
         <button class="button_landing">VISIT</button>
     </section>
