d07e46713770d9fa8913fdf73619425764cfe4cf
Marco Tiberio <45846191+marcotiberio@users.noreply.github.com>
2020-04-02 19:17:52 +0200

    4 columns footer

--- footer.php
+++ footer.php
@@ -35,6 +35,7 @@
 				<a href="mailto:info@gupmagazine.com" target="_blank">info@gupmagazine.com</a>
 				</p>
 			</div>
+			<div class="footer-custom-column" id="footer-custom-col3"></div>
 			<div class="footer-custom-column">
 				<h4>FOLLOW US</h4>
 				<div class="social-footer">
