08a0db1e77863e7686916d55df878447428c25e3
Marco Tiberio <45846191+marcotiberio@users.noreply.github.com>
2020-04-09 14:21:18 +0200

    x

--- latest posts.php
+++ latest posts.php
@@ -8,36 +8,31 @@
 		<main id="main" class="site-main">
 
 		<?php 
-			$args = array(
-				'post_type' => 'post',
-				'post_status' => 'publish',
-				'category_name' => 'wordpress',
+			// the query
+			$the_query = new WP_Query( array(
+				'category_name' => 'events',
 				'posts_per_page' => 5,
-			);
-			$arr_posts = new WP_Query( $args );
-			 
-			if ( $arr_posts->have_posts() ) :
-			 
-				while ( $arr_posts->have_posts() ) :
-					$arr_posts->the_post();
-					?>
-					<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-						<?php
-						if ( has_post_thumbnail() ) :
-							the_post_thumbnail();
-						endif;
-						?>
-						<header class="entry-header">
-							<h1 class="entry-title"><?php the_title(); ?></h1>
-						</header>
-						<div class="entry-content">
-							<?php the_excerpt(); ?>
-							<a href="<?php the_permalink(); ?>">Read More</a>
-						</div>
-					</article>
-					<?php
-				endwhile;
-			endif; ?>
+			)); 
+			?>
+
+			<?php if ( $the_query->have_posts() ) : ?>
+			<?php while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
+
+			<?php
+            if ( has_post_thumbnail() ) :
+                the_post_thumbnail();
+            endif;
+            ?>
+
+				<h3><?php the_title(); ?></h3>
+				<p><?php the_excerpt(); ?></p>
+
+			<?php endwhile; ?>
+			<?php wp_reset_postdata(); ?>
+
+			<?php else : ?>
+			<p><?php __('No Events'); ?></p>
+			<?php endif; ?>
 		
 
 		</main><!-- #main -->
