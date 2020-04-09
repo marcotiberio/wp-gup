8df1711e9d49863e8f6c16297c5512c236ba3375
Marco Tiberio <45846191+marcotiberio@users.noreply.github.com>
2020-04-09 14:59:14 +0200

    restore page main template

--- page-main-custom.php
+++ page-main-custom.php
@@ -1,5 +1,5 @@
 <?php 
-	/* Template Name: Test Latest Posts */ 
+	/* Template Name: Page Main */ 
 ?>
 
 <?php get_header(); ?>
@@ -7,42 +7,19 @@
 	<div id="primary--page-main" class="content-area--page-main">
 		<main id="main--page-main" class="site-main--page-main">
 
-		<?php 
-			$args = array(
-				'post_type' => 'post',
-				'post_status' => 'publish',
-				'category_name' => 'events',
-				'posts_per_page' => 20,
-			);
-			$arr_posts = new WP_Query( $args );
-			 
-			if ( $arr_posts->have_posts() ) :
-			 
-				while ( $arr_posts->have_posts() ) :
-					$arr_posts->the_post();
-					?>
-					<article class="latestpost--custom" id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-						<div>
-						<?php
-						if ( has_post_thumbnail() ) :
-							the_post_thumbnail();
-						endif;
-						?>
-						</div>
-						<div>
-						<header class="entry-header">
-							<h3 class="post-title"><?php the_title(); ?></h3>
-						</header>
-						<div class="entry-content">
-							<?php the_excerpt(); ?>
-							<a href="<?php the_permalink(); ?>">Read More</a>
-						</div>
-						</div>
-					</article>
-					<?php
-				endwhile;
-			endif; ?>
-		
+		<?php
+		while ( have_posts() ) :
+			the_post();
+
+			get_template_part( 'template-parts/content', 'page' );
+
+			// If comments are open or we have at least one comment, load up the comment template.
+			if ( comments_open() || get_comments_number() ) :
+				comments_template();
+			endif;
+
+		endwhile; // End of the loop.
+		?>
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
