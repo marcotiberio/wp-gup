7fbcb7e070ffca9ec604f28183b8972cce5bc296
Marco Tiberio <45846191+marcotiberio@users.noreply.github.com>
2020-04-09 13:34:07 +0200

    test

--- latest posts.php
+++ latest posts.php
@@ -7,7 +7,26 @@
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main">
 
-		<?php wpb_postsbycategory() ?>
+		<?php 
+			// the query
+			$the_query = new WP_Query( array(
+				'category_name' => 'events',
+				'posts_per_page' => 5,
+			)); 
+			?>
+
+			<?php if ( $the_query->have_posts() ) : ?>
+			<?php while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
+
+				<?php the_title(); ?>
+				<?php the_excerpt(); ?>
+
+			<?php endwhile; ?>
+			<?php wp_reset_postdata(); ?>
+
+			<?php else : ?>
+			<p><?php __('No Events'); ?></p>
+			<?php endif; ?>
 		
 
 		</main><!-- #main -->
