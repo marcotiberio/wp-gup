145c42104a5effc79dee63857e24a34606370270
Marco Tiberio <45846191+marcotiberio@users.noreply.github.com>
2020-04-09 13:27:42 +0200

    test

--- latest posts.php
+++ latest posts.php
@@ -7,16 +7,7 @@
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main">
 
-		<?php $catquery = new WP_Query( 'cat=7&posts_per_page=5' ); ?>
-		<ul>
-		<?php while($catquery->have_posts()) : $catquery->the_post(); ?>
-		<li><h3><a href="<?php the_permalink() ?>" rel="bookmark"><?php the_title(); ?></a></h3>
-		<ul><li><?php the_content(); ?></li>
-		</ul>
-		</li>
-		<?php endwhile; ?> 
-		</ul>
-		<?php wp_reset_postdata(); ?>
+		<?php wpb_postsbycategory() ?>
 		
 
 		</main><!-- #main -->
