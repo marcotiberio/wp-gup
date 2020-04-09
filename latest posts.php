3b34e740b18109c0c162d6a28b0706a75f593d20
Marco Tiberio <45846191+marcotiberio@users.noreply.github.com>
2020-04-09 14:51:34 +0200

    x

--- latest posts.php
+++ latest posts.php
@@ -1,54 +1,56 @@
+<?php 
+	/* Template Name: Test Latest Posts */ 
+?>
+
+<?php get_header(); ?>
+
+	<div id="primary--page-main" class="content-area--page-main">
+		<main id="main--page-main" class="site-main--page-main">
+			
+			<header class="entry-header">
+				<h1 class="entry-title"><?php the_title(); ?></h1>
+			</header>
+
+		<?php 
+			$args = array(
+				'post_type' => 'post',
+				'post_status' => 'publish',
+				'category_name' => 'events',
+				'posts_per_page' => 5,
+			);
+			$arr_posts = new WP_Query( $args );
+			 
+			if ( $arr_posts->have_posts() ) :
+			 
+				while ( $arr_posts->have_posts() ) :
+					$arr_posts->the_post();
+					?>
+					<article class="latestpost--custom" id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+						<div>
+						<?php
+						if ( has_post_thumbnail() ) :
+							the_post_thumbnail();
+						endif;
+						?>
+						</div>
+						<div>
+						<header class="entry-header">
+							<h3 class="post-title"><?php the_title(); ?></h3>
+						</header>
+						<div class="entry-content">
+							<?php the_excerpt(); ?>
+							<a href="<?php the_permalink(); ?>">Read More</a>
+						</div>
+						</div>
+					</article>
+					<?php
+				endwhile;
+			endif; ?>
+		
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
 <?php
-/**
- * Template Name: Test Latest Posts
- */
- 
-get_header(); ?>
- 
-<div id="primary" class="content-area">
-    <main id="main" class="site-main" role="main">
- 
-    <?php
-    $paged = (get_query_var( 'paged' )) ? get_query_var( 'paged' ) : 1;
-    $args = array(
-        'post_type' => 'post',
-        'post_status' => 'publish',
-        'category_name' => 'events',
-        'posts_per_page' => 5,
-        'paged' => $paged,
-    );
-    $arr_posts = new WP_Query( $args );
- 
-    if ( $arr_posts->have_posts() ) :
- 
-        while ( $arr_posts->have_posts() ) :
-            $arr_posts->the_post();
-            ?>
-            <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-                <?php
-                if ( has_post_thumbnail() ) :
-                    the_post_thumbnail();
-                endif;
-                ?>
-                <header class="entry-header">
-                    <h1 class="entry-title"><?php the_title(); ?></h1>
-                </header>
-                <div class="entry-content">
-                    <?php the_excerpt(); ?>
-                    <a href="<?php the_permalink(); ?>">Read More</a>
-                </div>
-            </article>
-            <?php
-        endwhile;
-        wp_pagenavi(
-            array(
-                'query' => $arr_posts,
-            )
-        );
-    endif;
-    ?>
- 
-    </main><!-- .site-main -->
-</div><!-- .content-area -->
- 
-<?php get_footer(); ?>
+get_sidebar();
+get_footer();
