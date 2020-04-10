89546febc4e5037899eaaae307efadea32f78a17
Marco Tiberio <45846191+marcotiberio@users.noreply.github.com>
2020-04-10 14:22:12 +0200

    x

--- latest-posts-archive.php
+++ latest-posts-archive.php
@@ -16,7 +16,7 @@
 				'post_type' => 'post',
 				'post_status' => 'publish',
 				'category_name' => 'archive',
-				'posts_per_page' => 200,
+				'posts_per_page' => 20,
 			);
 			$arr_posts = new WP_Query( $args );
 			 
