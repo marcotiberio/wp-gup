0afdf25c631398a3d42089e63ededd1b5c872c7f
Marco Tiberio <45846191+marcotiberio@users.noreply.github.com>
2020-04-10 14:43:24 +0200

    test search page

--- search.php
+++ search.php
@@ -24,28 +24,41 @@ get_header();
 				</h1>
 			</header><!-- .page-header -->
 
-			<?php
-			/* Start the Loop */
-			while ( have_posts() ) :
-				the_post();
-
-				/**
-				 * Run the loop for the search to output the results.
-				 * If you want to overload this in a child theme then include a file
-				 * called content-search.php and that will be used instead.
-				 */
-				get_template_part( 'template-parts/content-archive', 'search' );
-
-			endwhile;
-
-			the_posts_navigation();
-
-		else :
-
-			get_template_part( 'template-parts/content', 'none' );
-
-		endif;
-		?>
+			<?php 
+			$args = array(
+				'post_type' => 'post',
+				'post_status' => 'publish',
+				'posts_per_page' => 20,
+			);
+			$arr_posts = new WP_Query( $args );
+			 
+			if ( $arr_posts->have_posts() ) :
+			 
+				while ( $arr_posts->have_posts() ) :
+					$arr_posts->the_post();
+					?>
+					<article class="latestpost--custom" id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+						<div>
+						<?php
+						if ( has_post_thumbnail() ) :
+							the_post_thumbnail();
+						endif;
+						?>
+						</div>
+						<div>
+						<header class="entry-header">
+							<a href="<?php the_permalink(); ?>"><h3 class="post-title"><?php the_title(); ?></h3></a>
+							<h5 class="post-subtitle"><?php the_field('post_subtitle'); ?></h5>
+						</header>
+						<div class="entry-content">
+							<?php the_excerpt(); ?>
+							<a href="<?php the_permalink(); ?>">Read More</a>
+						</div>
+						</div>
+					</article>
+					<?php
+				endwhile;
+			endif; ?>
 
 		</main><!-- #main -->
 	</section><!-- #primary -->
