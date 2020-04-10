4bf9f7fa92b7fe8da4c2d5ab8c76a0b91469338c
Marco Tiberio <45846191+marcotiberio@users.noreply.github.com>
2020-04-10 09:37:59 +0200

    latest post archive template

---
+++ latest-posts-archive.php
@@ -0,0 +1,57 @@
+<?php 
+	/* Template Name: Latest Posts Archive */ 
+?>
+
+<?php get_header(); ?>
+
+	<div id="primary--page-main" class="content-area--page-main">
+		<main id="main--page-main" class="site-main--page-main">
+			
+			<header class="entry-header">
+				<h1 class="entry-title"><?php the_title(); ?></h1>
+			</header>
+
+		<?php 
+			$args = array(
+				'post_type' => 'post',
+				'post_status' => 'publish',
+				'category_name' => 'archive',
+				'posts_per_page' => 200,
+			);
+			$arr_posts = new WP_Query( $args );
+			 
+			if ( $arr_posts->have_posts() ) :
+			 
+				while ( $arr_posts->have_posts() ) :
+					$arr_posts->the_post();
+					?>
+					<article class="latestpost--custom" id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+						<div>
+						<?php
+						if ( has_post_thumbnail() ) :
+							the_post_thumbnail();
+						endif;
+						?>
+						</div>
+						<div>
+						<header class="entry-header">
+							<a href="<?php the_permalink(); ?>"><h3 class="post-title"><?php the_title(); ?></h3></a>
+							<h5 class="post-subtitle"><?php the_field('post_subtitle'); ?></h5>
+						</header>
+						<div class="entry-content">
+							<?php the_excerpt(); ?>
+							<a href="<?php the_permalink(); ?>">Read More</a>
+						</div>
+						</div>
+					</article>
+					<?php
+				endwhile;
+			endif; ?>
+		
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php
+get_sidebar();
+get_footer();
