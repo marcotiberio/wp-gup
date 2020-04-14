b8a8afce69d529a4463a41c53be11e28d3cf85ad
Marco Tiberio <45846191+marcotiberio@users.noreply.github.com>
2020-04-14 17:04:37 +0200

    mobile menu icon

--- header.php
+++ header.php
@@ -46,7 +46,7 @@
 		
 		<div class="nav-custom">
 			<nav id="site-navigation" class="main-navigation">
-				<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'gup_underscore' ); ?></button>
+				<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><i class="fas fa-bars"></i></button>
 				<?php
 				wp_nav_menu( array(
 					'theme_location' => 'menu-1',
