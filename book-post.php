c65f3503fc147092e18dd3025f0972385ae7876d
Marco Tiberio <45846191+marcotiberio@users.noreply.github.com>
2020-04-20 16:05:57 +0200

    test post book

--- book-post.php
+++ book-post.php
@@ -7,11 +7,11 @@
  get_header();  ?>
 
 <div class="wrap">
-    
-    <div id="primary" class="content-area">
-		<main id="main" class="site-main">
 
-		<?php
+    <div id="primary" class="content-area">
+        <main id="main" class="site-main">
+
+            <?php
 		while ( have_posts() ) :
 			the_post();
 
@@ -27,89 +27,100 @@
 		endwhile; // End of the loop.
 		?>
 
-		</main><!-- #main -->
-	</div><!-- #primary -->
-    
+        </main><!-- #main -->
+    </div><!-- #primary -->
+
     <!-- CUSTOM POST LAYOUT -->
     <!-- CUSTOM POST LAYOUT -->
     <!-- CUSTOM POST LAYOUT -->
+
 
     <div id="post-title">
         <h2><?php the_field('book_title'); ?></h2>
     </div>
 
     <hr style="margin: 0 10%;">
-    
+
     <div id="post-content">
         <div class="post-credits">
             <div>
                 <?php if( get_field('book_credits_author') ): ?>
-                    <h4>GUP Author</h4>
-                    <p><?php the_field('book_credits_author'); ?></h4></p>
+                <h4>GUP Author</h4>
+                <p><?php the_field('book_credits_author'); ?></h4>
+                </p>
                 <?php endif; ?>
             </div>
             <hr style="margin-right:40%;">
             <div>
                 <?php if( get_field('book_credits_artist') ): ?>
-                    <h4>Artist</h4>
-                    <p><?php the_field('book_credits_artist'); ?></h4></p>
+                <h4>Artist</h4>
+                <p><?php the_field('book_credits_artist'); ?></h4>
+                </p>
                 <?php endif; ?>
             </div>
             <div>
                 <?php if( get_field('book_credits_artist-website') ): ?>
-                    <h4>Artist Website</h4>
-                    <p><?php the_field('book_credits_artist-website'); ?></h4></p>
+                <h4>Artist Website</h4>
+                <p><?php the_field('book_credits_artist-website'); ?></h4>
+                </p>
                 <?php endif; ?>
             </div>
             <div>
                 <?php if( get_field('book_credits_artist-social-media') ): ?>
-                    <h4>Artist Social Media</h4>
-                    <p><?php the_field('book_credits_artist-social-media'); ?></h4></p>
+                <h4>Artist Social Media</h4>
+                <p><?php the_field('book_credits_artist-social-media'); ?></h4>
+                </p>
                 <?php endif; ?>
             </div>
             <hr style="margin-right:40%;">
             <div>
                 <?php if( get_field('book_credits_book-title') ): ?>
-                    <h4>Title</h4>
-                    <p><?php the_field('book_credits_book-title'); ?></h4></p>
-                    <?php if( get_field('book_credits_book-cover') ): ?>
-                        <?php 
+                <h4>Title</h4>
+                <p><?php the_field('book_credits_book-title'); ?></h4>
+                </p>
+                <?php if( get_field('book_credits_book-cover') ): ?>
+                <?php 
                         $image = get_field('book_credits_book-cover');
                         $size = 'thumbnail'; // (thumbnail, medium, large, full or custom size)
                         if( $image ) {
                             echo wp_get_attachment_image( $image, $size );
                         } ?>
-                    <?php endif; ?>
+                <?php endif; ?>
                 <?php endif; ?>
             </div>
             <div>
                 <?php if( get_field('book_credits_book-publisher') ): ?>
-                    <h4>Publisher</h4>
-                    <p><?php the_field('book_credits_book-publisher'); ?></h4></p>
+                <h4>Publisher</h4>
+                <p><?php the_field('book_credits_book-publisher'); ?></h4>
+                </p>
                 <?php endif; ?>
             </div>
             <div>
                 <?php if( get_field('book_credits_book-format') ): ?>
-                    <h4>Format</h4>
-                    <p><?php the_field('book_credits_book-format'); ?></h4></p>
+                <h4>Format</h4>
+                <p><?php the_field('book_credits_book-format'); ?></h4>
+                </p>
                 <?php endif; ?>
             </div>
             <div>
                 <?php if( get_field('book_credits_book-price') ): ?>
-                    <h4>Price</h4>
-                    <p><?php the_field('book_credits_book-price'); ?></h4></p>
+                <h4>Price</h4>
+                <p><?php the_field('book_credits_book-price'); ?></h4>
+                </p>
                 <?php endif; ?>
             </div>
             <div>
                 <?php if( get_field('book_credits_publisher-website') ): ?>
-                    <h4>Publisher Website</h4>
-                    <p><?php the_field('book_credits_publisher-website'); ?></h4></p>
+                <h4>Publisher Website</h4>
+                <p><?php the_field('book_credits_publisher-website'); ?></h4>
+                </p>
                 <?php endif; ?>
             </div>
             <div>
                 <?php if( get_field('book_credits_publisher-social-media') ): ?>
-                    <h4>Publisher Social Media</h4>
-                    <p><?php the_field('book_credits_publisher-social-media'); ?></h4></p>
+                <h4>Publisher Social Media</h4>
+                <p><?php the_field('book_credits_publisher-social-media'); ?></h4>
+                </p>
                 <?php endif; ?>
             </div>
         </div>
@@ -117,32 +128,33 @@
         <div class="post-text">
             <p><?php the_field('book_text'); ?></p>
         </div>
-		
+
     </div>
-	
+
+
 </div>
 
 <hr style="margin: 0 10%;">
 
 <?php $orig_post = $post;
-global $post;
-$categories = get_the_category($post->ID);
-if ($categories) {
-$category_ids = array();
-foreach($categories as $individual_category) $category_ids[] = $individual_category->term_id;
+    global $post;
+    $categories = get_the_category($post->ID);
+    if ($categories) {
+    $category_ids = array();
+    foreach($categories as $individual_category) $category_ids[] = $individual_category->term_id;
 
-$args=array(
-'category__in' => $category_ids,
-'post__not_in' => array($post->ID),
-'posts_per_page'=> 2, // Number of related posts that will be shown.
-'ignore_sticky_posts'=>1
-);
+    $args=array(
+        'category__in' => $category_ids,
+        'post__not_in' => array($post->ID),
+        'posts_per_page'=> 2, // Number of related posts that will be shown.
+        'ignore_sticky_posts'=>1
+    );
 
-$my_query = new wp_query( $args );
-if( $my_query->have_posts() ) {
-echo '<div id="related_posts"><h3>Related Posts</h3><div class="related_posts-grid">';
-while( $my_query->have_posts() ) {
-$my_query->the_post();?>
+    $my_query = new wp_query( $args );
+    if( $my_query->have_posts() ) {
+    echo '<div id="related_posts"><h3>Related Posts</h3><div class="related_posts-grid">';
+    while( $my_query->have_posts() ) {
+    $my_query->the_post();?>
 
     <div class="relatedthumb">
         <a href="<? the_permalink()?>" rel="bookmark" title="<?php the_title(); ?>"><?php the_post_thumbnail(); ?></a>
@@ -151,12 +163,17 @@ $my_query->the_post();?>
             <p><?php the_excerpt(); ?></p>
         </div>
     </div>
-<?
-}
-echo '</div></div>';
-}
-}
-$post = $orig_post;
-wp_reset_query(); ?>
+    <?
+    }
+    echo '</div></div>';
+    }
+    }
+    $post = $orig_post;
+    wp_reset_query(); ?>
+
+    }
+
+    </main><!-- #main -->
+    </div><!-- #primary -->
 
 <?php get_footer(); ?>
